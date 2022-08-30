1ecaeba35b6adc7c364a01ede8c714f83d2b503d
Jonathan Fischoff <jonathangfischoff@gmail.com>
2022-08-30 13:25:00 -0700

    update policies to V2

--- src/Canonical/TestMinting.hs
+++ src/Canonical/TestMinting.hs
@@ -1,25 +1,57 @@
 module Canonical.TestMinting
   ( mintingAsCbor
+  , alwaysSucceedsMintingPolicy
   ) where
 import           PlutusTx.Prelude
-import           Ledger.Typed.Scripts
-import           Ledger
 import           PlutusTx
 import qualified Data.ByteString.Lazy     as LB
 import qualified Data.ByteString.Short    as SBS
-import           Cardano.Api.Shelley      (PlutusScript (..), PlutusScriptV1)
+import           Cardano.Api.Shelley      (PlutusScript (..), PlutusScriptV2)
 import           Codec.Serialise
+import           Plutus.V2.Ledger.Contexts
+import           Plutus.V1.Ledger.Scripts
+import qualified Cardano.Api.Shelley as Shelly
+import qualified Data.ByteString.Short as BSS
+import qualified Data.ByteString.Lazy as BSL
 
 {-# INLINABLE alwaysSucceed #-}
 alwaysSucceed :: Integer -> BuiltinData -> ScriptContext -> Bool
 alwaysSucceed x _ _ = x == x
 
+mintingPolicyHash :: MintingPolicy -> MintingPolicyHash
+mintingPolicyHash
+  = MintingPolicyHash
+  . getScriptHash
+  . scriptHash
+  . getValidator
+  . Validator
+  . getMintingPolicy
+
+wrappedPolicy :: Integer -> BuiltinData -> BuiltinData -> ()
+wrappedPolicy i x y = check $ alwaysSucceed i x (unsafeFromBuiltinData y)
+
 alwaysSucceedsMintingPolicy :: Integer -> MintingPolicy
 alwaysSucceedsMintingPolicy num = mkMintingPolicyScript $
-  $$(compile [|| \(i :: Integer) -> wrapMintingPolicy $ alwaysSucceed i ||])
+  $$(compile [|| wrappedPolicy ||])
     `applyCode` liftCode num
 
-mintingAsCbor :: Integer -> PlutusScript PlutusScriptV1
+scriptHash :: Script -> ScriptHash
+scriptHash =
+    ScriptHash
+    . toBuiltin
+    . Shelly.serialiseToRawBytes
+    . Shelly.hashScript
+    . toCardanoApiScript
+
+toCardanoApiScript :: Script -> Shelly.Script Shelly.PlutusScriptV2
+toCardanoApiScript
+  = Shelly.PlutusScript Shelly.PlutusScriptV2
+  . Shelly.PlutusScriptSerialised
+  . BSS.toShort
+  . BSL.toStrict
+  . serialise
+
+mintingAsCbor :: Integer -> PlutusScript PlutusScriptV2
 mintingAsCbor
   = PlutusScriptSerialised
   . SBS.toShort
